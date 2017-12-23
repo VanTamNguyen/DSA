67f059dba7fdcbabcd44e1142b63b4b0d07e6e8e
VanTamNguyen <vantamnguyen.vnu@gmail.com>
2017-12-23 23:29:25 +0700

    Merge sort recursive

--- code/dsa/src/com/tamco/dsa/sorting/MergeSort.java
+++ code/dsa/src/com/tamco/dsa/sorting/MergeSort.java
@@ -1,26 +1,49 @@
 package com.tamco.dsa.sorting;
 
+import java.util.Arrays;
+
 public class MergeSort {
 
     public static void main(String[] args) {
-
+        int[] array = {0, 2, 5, 6, 9, 1, 5, 9, 9, 9};
+        merge(array, 0, 4, 9);
+        System.out.println(Arrays.toString(array));
     }
 
-    public static void merge(SortableItem[] items, int low, int mid, int high) {
+    public static void merge(int[] items, int low, int mid, int high) {
 
-        SortableItem[] auxiliary = new SortableItem[high - low + 1];
+        int[] aux = new int[high - low + 1];
         for (int i = low; i <= high; i++) {
-            auxiliary[i - low] = items[i];
+            aux[i - low] = items[i];
         }
 
         int i = 0, j = mid - low + 1;
-        while (i < mid - low + 1 && j < high - low + 1) {
-            while (auxiliary[i].lessThan(auxiliary[j])) {
-                items[i + low] = auxiliary[i];
-                i++;
+        int index = low;
+
+        while (index <= high) {
+            if (i >= mid - low + 1) {
+                items[index] = aux[j];
+                j++;
+                index++;
+                continue;
             }
 
-//            while ()
+            if (j >= high - low + 1) {
+                items[index] = aux[i];
+                i++;
+                index++;
+                continue;
+            }
+
+            if (aux[i] <= aux[j]) {
+                items[index] = aux[i];
+                i++;
+
+            } else {
+                items[index] = aux[j];
+                j++;
+            }
+            index++;
         }
     }
 }
